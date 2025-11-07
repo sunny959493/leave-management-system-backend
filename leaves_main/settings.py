844e34c3d872dbeecafa21a468f6b8f1f01a51c6
sunny959493 <vsunnihithreddy@gmai.com>
2025-11-07 12:22:04 +0530

    changed page_size

--- leaves_main/settings.py
+++ leaves_main/settings.py
@@ -137,7 +137,7 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 3,
+    'PAGE_SIZE': 20,
     # 'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
 }
 
